3452f6086629895a24e8281a221b5741c9657a08
Georg Vienna <georg.vienna@aalto.fi>
2015-02-25 19:37:15 +0200

    basic function and assets

---
+++ Assets/ourcollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ourcollision : MonoBehaviour {
+	void OnCollisionEnter(Collision collision) {
+		foreach (ContactPoint contact in collision.contacts) {
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+		if (collision.relativeVelocity.magnitude > 2)
+			audio.Play();
+		Debug.Log ("AAAAAAAAAAAAAAAAAAAAAAAAAAA");
+	}
+}
