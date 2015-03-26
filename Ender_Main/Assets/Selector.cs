2a94a2085561322baaebda373b26943bdede0aba
Georg Vienna <georg.vienna@aalto.fi>
2015-03-26 15:12:30 +0200

    Proximity works, selection needs some more work

---
+++ Ender_Main/Assets/Selector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selector : MonoBehaviour {
+
+	public GameObject selection;
+
+	// Use this for initialization
+	void Start () {
+		renderer.enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * Time.deltaTime * 10, Space.World);
+		renderer.enabled = false;
+
+		if(selection != null){
+			renderer.enabled = true;
+			transform.position = selection.transform.position;
+		}
+	}
+}
---
+++ Ender_Main/Assets/WandCollider.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WandCollider : MonoBehaviour {
+
+	GameObject selection = null;
+
+	Selector selector;
+
+	void Start(){
+		selector = (Selector)GameObject.Find("Selector_Sphere").GetComponent("Selector");
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Trigger----------------------------------");
+		if (selection != null) {
+			float  ms = Vector3.Distance(gameObject.transform.position, selection.transform.position);
+			float  mo = Vector3.Distance(gameObject.transform.position, other.gameObject.transform.position);
+			if(mo < ms)
+				SetSelection(other.gameObject);
+		}
+		else {
+			SetSelection(other.gameObject);
+		}
+	
+	}
+
+	void SetSelection(GameObject select){
+		this.selection = select;
+		selector.selection = select;
+	}
+
+
+	void OnTriggerStay(Collider other) {
+		Debug.Log ("Trigger Stay----------------------------------");
+		if(selection == null)
+			SetSelection(other.gameObject);
+		else if (other.gameObject != selection) {
+			float  ms = Vector3.Distance(gameObject.transform.position, selection.transform.position);
+			float  mo = Vector3.Distance(gameObject.transform.position, other.gameObject.transform.position);
+			if(mo < ms)
+				SetSelection(other.gameObject);
+		}
+		
+	}
+	void OnTriggerExit(Collider other) {
+		Debug.Log ("Trigger Stay----------------------------------");
+		if (other.gameObject == selection)
+			SetSelection(null);
+		
+	}
+}
