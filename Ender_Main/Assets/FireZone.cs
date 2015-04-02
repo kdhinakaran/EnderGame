5fb35960c4451945a604d7523ef09063865d859a
Georg Vienna <georg.vienna@aalto.fi>
2015-04-02 13:54:26 +0300

    mouse wand scene

---
+++ Ender_Main/Assets/FireZone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireZone : MonoBehaviour {
+	public GameObject firezone;
+	// Use this for initialization
+	void Start () {
+		Vector3 position = transform.position + transform.TransformPoint(new Vector3(-0.5f, 0, 0));
+		GameObject fire = (GameObject)Instantiate (firezone, position, transform.rotation);
+		fire.transform.localScale = new Vector3 (0.01f, 0.01f, 0.01f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
