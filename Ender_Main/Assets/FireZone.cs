bd6fc2f5c31df54b2b0b02dd7ff61be075dfa5ea
Georg Vienna <georg.vienna@aalto.fi>
2015-04-16 14:17:07 +0300

    basic weapons implemented, new firezone

--- Ender_Main/Assets/FireZone.cs
+++ Ender_Main/Assets/FireZone.cs
@@ -12,10 +12,18 @@ public class FireZone : MonoBehaviour {
 	public Color c1 = Color.yellow;
 	public Color c2 = Color.red;
 	private float rotationValue;
+
+	private DetectShips detectShips;
+	private FireMissile fireMissile;
+
+	private Color first = new Color(120f/255f, 75f/255f, 55f/255f, 255f/255f);
+	private Color second = new Color(170f/255f, 100f/255f, 25f/255f, 255f/255f);
 	// Use this for initialization
 	void Start () {
 		Vector3 position = transform.TransformPoint(new Vector3(0, -0.5f, 0));
 		zone = (GameObject)Instantiate (firezone, position, transform.rotation);
+		detectShips = (DetectShips)zone.GetComponent("DetectShips");
+		fireMissile = (FireMissile)GetComponent ("FireMissile");
 
 		lineRenderer = lineRender.GetComponent<LineRenderer>();
 		lineRenderer.SetWidth(0.01F, 0.01F);
@@ -26,17 +34,29 @@ public class FireZone : MonoBehaviour {
 		rotationValue = Random.value;
 	}
 	
+	float period = 4;
+	float time = 0;
 	// Update is called once per frame
 	void Update () {
 		lineRenderer.SetPosition(0, transform.position);
 		lineRenderer.SetPosition(1, zone.transform.position);
-		if (Vector3.Distance (transform.position, zone.transform.position) > 5) {
+		bool inRange = Vector3.Distance (transform.position, zone.transform.position) < 5;
+		if (!inRange) {
 			lineRenderer.SetColors(Color.gray, Color.gray);
 		}
 		else
 			lineRenderer.SetColors(c1, c2);
 
-		
 		zone.transform.Rotate(new Vector3(0.5f,0.3f,rotationValue) * Time.deltaTime * 10, Space.World);
+		
+		time += Time.deltaTime;
+		if (detectShips.ShipsInside () && inRange && time > period) {
+			fireMissile.Fire(zone.transform.position);
+			time = 0;
+		}
+		// Color.Lerp(first, second, time/period)
+		zone.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.white);
+		zone.GetComponent<Renderer> ().material.SetColor ("_Color", Color.white);
+		zone.GetComponent<Renderer> ().material.color = Color.white;
 	}
 }
---
+++ Ender_Main/Assets/Missile.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missile : MonoBehaviour {
+
+	public GameObject explosion;
+	float period, time;
+
+	private State state = State.IDLE;
+	
+	enum State {
+		IDLE,
+		MOVE,
+		EXPLODE
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		switch (state) {
+		case State.IDLE:
+			break;
+		case State.MOVE:
+			{
+				time += Time.fixedDeltaTime;
+				if (time >= period) {
+					state = State.EXPLODE;
+					time = 0;
+				}
+				break;
+			}
+			case State.EXPLODE:
+			{
+			Explode();
+				break;
+			}	
+		}
+	}
+
+	public void SetTarget(Vector3 target){
+		state = State.MOVE;
+		
+		period = 20;
+		time = 0;
+		
+		Vector3 vec = target - transform.position;
+		GetComponent<Rigidbody> ().AddForce (vec*100, ForceMode.Acceleration);
+
+		transform.LookAt (target);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		foreach (ContactPoint contact in collision.contacts) {
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+		Debug.Log ("OnCollisionEnter");
+	}
+
+	void Explode(){
+		Instantiate (explosion, transform.position, transform.rotation);
+		Destroy(gameObject);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("EnemyShip"))
+			Explode ();
+		else if(other.gameObject.tag.Equals ("Missile")) {
+			Explode();
+			Destroy(other.gameObject);
+		}
+	}
+}
