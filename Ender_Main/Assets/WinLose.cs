88962886ae92b87958bcae171807915ad6ec793c
Georg Vienna <georg.vienna@aalto.fi>
2015-05-04 18:49:04 +0300

    added indicator model, selection and firezone sounds

--- Ender_Main/Assets/WinLose.cs
+++ Ender_Main/Assets/WinLose.cs
@@ -3,7 +3,9 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class WinLose : MonoBehaviour {
-	
+	public RUISPSMoveWand wand1;
+	public RUISPSMoveWand wand2;
+
 	public Text text;
 	// Use this for initialization
 	void Start () {
@@ -18,5 +20,7 @@ public class WinLose : MonoBehaviour {
 		} else if (allied.Length == 0){
 			text.text = "Game Over! \nYou Lose!";
 		}
+		if(wand1.startButtonWasPressed || wand2.startButtonWasPressed)
+			Application.LoadLevel(Application.loadedLevel);
 	}
 }
